fcfa4a256318159faae729d28f40cf8cd21c4e6d
urdudewithkali <128925737+urdudewithkali@users.noreply.github.com>
2024-04-30 13:43:21 +0200

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '1cfb8c88-33d3-4cc5-a061-72ed6d2eafe6';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
